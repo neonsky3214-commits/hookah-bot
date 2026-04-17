83c18daf8178bc901b17de1e1749e503bca286ad
neonsky3214-commits <neonsky3214@gmail.com>
2026-04-17 23:07:17 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "0"))
-ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "0").split(",")]
