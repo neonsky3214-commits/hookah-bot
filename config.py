9c9b26c9559c6a29f3c50c4c9229c505e4075b38
neonsky3214-commits <neonsky3214@gmail.com>
2026-04-17 22:49:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.environ.get("8777411837:AAElTBxbr6IBmVaW4JU6qpDZhypieZ_wHGI", "")
-ADMIN_CHAT_ID = int(os.environ.get("-1003658423562", "0"))
-ADMIN_IDS = [int(x) for x in os.environ.get("520032441", "0").split(",")]
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "0"))
+ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "0").split(",")]
