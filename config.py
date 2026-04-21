e575ebee6ac38ff4f2b47b9ddf9f672b0953f9e5
neonsky3214-commits <neonsky3214@gmail.com>
2026-04-21 11:30:50 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "0"))
-
-# URL где живёт твой Mini App — заполнится после деплоя на Railway
-WEBAPP_URL = os.environ.get("WEBAPP_URL", "")
